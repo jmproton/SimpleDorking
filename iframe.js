f985536367c50088845c68d7ac07d53ab5e7352f
jmproton <97969196+jmproton@users.noreply.github.com>
2022-03-23 15:19:57 -0400

    Add edPile

---
+++ iframe.js
@@ -0,0 +1,17 @@
+myWebResultsRenderedCallback = function() {
+    window.parent.postMessage({id:window['iframeId'], height:document.body.scrollHeight});
+};
+window.__gcse || (window.__gcse = {});
+window.__gcse.searchCallbacks = {
+    web: {
+        rendered: 'myWebResultsRenderedCallback',
+    },
+};
+function executeQuery(search) {
+    var element = google.search.cse.element.getElement('searchresults-only0');
+    if (search == '') {
+        element.clearAllResults();
+    } else {
+        element.execute(search);
+    }
+}
